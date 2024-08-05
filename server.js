ab0cb44fcc68f182ae4ad8985dc5670c5ba53640
flasky06 <bonnienjuguna106@gmail.com>
2024-08-05 03:36:12 +0300

    db configs

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { Pool } = require("pg");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 8080;
